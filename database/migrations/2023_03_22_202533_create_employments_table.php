f5d2142da69d696ec8306b4311c30bd7bc7dd285
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-03-28 15:46:26 -0400

    Create 2023_03_22_202533_create_employments_table.php

---
+++ database/migrations/2023_03_22_202533_create_employments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('employer');
+            $table->timestamp('started_at');
+            $table->timestamp('ended_at')->nullable();
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employments');
+    }
+};

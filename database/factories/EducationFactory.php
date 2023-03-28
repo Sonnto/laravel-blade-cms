38b685573a2ccd9c48a571fedf4d16500df193f1
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-03-28 15:45:55 -0400

    Create EducationFactory.php, add fake data

---
+++ database/factories/EducationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Education>
+ */
+class EducationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'institute' => $this->faker->sentence,
+            'qualification' => $this->faker->sentence,
+            'started_at' => $this->faker->dateTimeThisMonth,
+            'ended_at' => $this->faker->dateTimeThisMonth,
+            'content' => $this->faker->paragraph,
+            //
+        ];
+    }
+}

13ac478696d5af7be9d517395c5df3f7b500936d
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-01 14:08:57 -0400

    add links for entries, education, employments

--- resources/views/console/dashboard.blade.php
+++ resources/views/console/dashboard.blade.php
@@ -5,6 +5,9 @@
 <section class="w3-padding">
 
     <ul id="dashboard">
+        <li><a href="/console/education/list">Manage Education</a></li> (todo)
+        <li><a href="/console/employments/list">Manage Employments</a></li> (todo)
+        <li><a href="/console/entries/list">Manage Entries</a></li>
         <li><a href="/console/projects/list">Manage Projects</a></li>
         <li><a href="/console/types/list">Manage Types</a></li>
         <li><a href="/console/users/list">Manage Users</a></li>

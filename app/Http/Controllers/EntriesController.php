2867cc933d238751d6dad973d0ac7831100c59f7
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-03-31 20:20:09 -0400

    Remove created_at and updated_at

--- app/Http/Controllers/EntriesController.php
+++ app/Http/Controllers/EntriesController.php
@@ -14,4 +14,64 @@ class EntriesController extends Controller
             'entries' => Entry::all()
         ]);
     }
+    
+    public function add()
+    {
+
+        $attributes = request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'learnt_at' => 'required',
+        ]);
+
+        $entry = new Entry();
+        $entry->title = $attributes['title'];
+        $entry->content = $attributes['content'];
+        $entry->learnt_at = $attributes['learnt_at'];
+
+        $entry->user_id = Auth::user()->id;
+        $entry->save();
+
+        return redirect('/console/entries/list')
+            ->with('message', 'Entry has been added!');
+    }
+
+    public function editForm(Entry $entry)
+    {
+        return view('entries.edit', [
+            'entry' => $entry,
+        ]);
+    }
+
+    public function edit(Entry $entry)
+    {
+
+        $attributes = request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'learnt_at' => 'required',
+        ]);
+
+        $entry->title = $attributes['title'];
+        $entry->content = $attributes['content'];
+        $entry->learnt_at = $attributes['learnt_at'];
+        $entry->save();
+
+        return redirect('/console/entries/list')
+            ->with('message', 'Entry has been edited!');
+    }
+
+    public function delete(Entry $entry)
+    {
+
+        if($entry->image)
+        {
+            Storage::delete($entry->image);
+        }
+        
+        $entry->delete();
+        
+        return redirect('/console/entries/list')
+            ->with('message', 'Entry has been deleted!');        
+    }
 }

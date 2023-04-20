6776444c658288a8e18a9657414da8ce22a3b9ad
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-20 13:26:59 -0400

    Update Employment model with columns and user_id

--- app/Models/Employment.php
+++ app/Models/Employment.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Employment extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'employer',
+        'location',
+        'started_at',
+        'ended_at',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(USer::class, 'user_id');
+    }
 }

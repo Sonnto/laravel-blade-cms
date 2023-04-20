91a5963c5faffc12d3f693d5fa7871d9e31f7a20
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-20 13:27:04 -0400

    Update Education model with columns and user_id

--- app/Models/Education.php
+++ app/Models/Education.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Education extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'institute',
+        'qualification',
+        'location',
+        'started_at',
+        'ended_at',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(USer::class, 'user_id');
+    }
 }

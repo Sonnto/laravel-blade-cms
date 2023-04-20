6666099228b4bd139d7097f30151531ee80f1b17
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-20 15:40:14 -0400

    Add portfolio layout for frontend

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -1,24 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>My Portfolio | {{$title}}    </title>
-
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="{{url('app.css')}}">
-
-    <script src="{{url('app.js')}}"></script>
-    
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Portfolio website" />
+    <meta
+      name="keywords"
+      content="full-stack, developer, front-end, back-end, software, engineer, portfolio, HTML, CSS, JavaScript, MongoDB, Express.js, React.js, Node.js, SQL, mySQL, PHP, C#, Python, JQuery, Toronto, Ontario, Canada, Hong Kong, Cantonese"
+    />
+    <meta name="author" content="Kee-Fung Anthony Ho" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Anthony Ho | Full-Stack Developer</title>
+    <script
+      src="https://kit.fontawesome.com/fe76fcb7fd.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/normalize.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}" />
+    <meta name="theme-color" content="#fafafa" />
 </head>
 <body>
-
-<header class="w3-padding">
-
-    <h1 class="w3-text-red">My Portfolio!</h1>
-
-</header>
+    <header id="header">
+      <!--============================== NAV ==============================-->
+      <nav id="nav">
+        <div id="menu-tab"></div>
+        <div id="menu-toggle">
+          <input name="menu-button-click" type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+            <li><a href="#home">Home</a></li>
+            <li><hr class="nav-division" /></li>
+            <li><a href="#about">About</a></li>
+            <li><hr class="nav-division" /></li>
+            <li><a href="#resume">Resume</a></li>
+            <li><hr class="nav-division" /></li>
+            <li><a href="#projects">Projects</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
 
 <hr>
 
@@ -26,12 +48,9 @@
 
 <hr>
 
-<footer class="w3-padding">
+<footer id="footer">
 
-    Footer Text | 
-    Copyright {{date('Y')}}
-    <a href="#">Facebook</a> | 
-    <a href="#">Instagram</a>
+    © Copyright, Kee-Fung Anthony Ho, {{date('Y')}}
 
     <br>
 

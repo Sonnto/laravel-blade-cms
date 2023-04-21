bb1a80ee3d607b711cf7cd1a05ccd707de582be7
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-21 19:23:10 -0400

    Rename file ext. App.jsx

--- react-frontend-app/src/App.jsx
+++ react-frontend-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Resume from "./components/Resume";
 import Project from "./components/Project";
 import "./App.css";
 
 function App() {
     const [projects, setProjects] = useState(null);
-    // const [education, setEducation] = useState(null);
-    // const [employments, setEmployments] = useState(null);
+    const [education, setEducation] = useState(null);
+    const [employments, setEmployments] = useState(null);
 
     useEffect(() => {
         const fetchData = async (api, setter) => {
@@ -20,11 +21,23 @@ function App() {
             }
         };
         fetchData("projects", setProjects);
-        // fetchData("education", setEmployments);
-        // fetchData("employments", setEmployments);
+        fetchData("education", setEducation);
+        fetchData("employments", setEmployments);
     }, []);
 
-    return <div>{projects && <Project data={projects[0]} />}</div>;
+    return (
+        <>
+            <div>
+                {education && employments && (
+                    <Resume
+                        employmentData={employments[0]}
+                        educationData={education[0]}
+                    />
+                )}
+            </div>
+            <div className="">{projects && <Project data={projects[0]} />}</div>
+        </>
+    );
 }
 
 export default App;

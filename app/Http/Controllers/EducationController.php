5afab257999f170c1a347392d5cc200d243186f4
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-20 20:53:59 -0400

    Update EducationController.php

--- app/Http/Controllers/EducationController.php
+++ app/Http/Controllers/EducationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 use App\Models\Education;
 use Illuminate\Support\Carbon;
 

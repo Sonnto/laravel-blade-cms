1413ace8d38858a4ac1dd08f277500fd56554f38
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-21 19:51:14 -0400

    Split Resume component to Employment, Education

---
+++ react-frontend-app/src/components/Education.jsx
@@ -0,0 +1,9 @@
+function Education({ data }) {
+    return (
+        <div>
+            <h1>{data.institute}</h1>
+        </div>
+    );
+}
+
+export default Education;
---
+++ react-frontend-app/src/components/Employment.jsx
@@ -0,0 +1,9 @@
+function Employment({ data }) {
+    return (
+        <div>
+            <h1>{data.employer}</h1>
+        </div>
+    );
+}
+
+export default Employment;

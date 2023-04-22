672d1f5da7fca755202d6d91abb2a20b249faa5c
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-21 20:12:57 -0400

    Loop all data for Education

--- react-frontend-app/src/components/Education.jsx
+++ react-frontend-app/src/components/Education.jsx
@@ -1,8 +1,27 @@
 function Education({ data }) {
     return (
-        <div>
-            <h1>{data.institute}</h1>
-        </div>
+        <>
+            <h3 className="experience-name">{data.institute}</h3>
+            <h4>{data.location}</h4>
+            <h4>
+                {new Date(data.started_at).toLocaleDateString("en-US", {
+                    month: "short",
+                    year: "numeric",
+                })}
+                {data.ended_at
+                    ? ` - ${new Date(data.ended_at).toLocaleDateString(
+                          "en-US",
+                          { month: "short", year: "numeric" }
+                      )}`
+                    : " - Present"}
+            </h4>
+            <h4>{data.qualification}</h4>
+            <ul>
+                {data.content.split(";").map((bullet, index) => (
+                    <li key={index}>{bullet}</li>
+                ))}
+            </ul>
+        </>
     );
 }
 
